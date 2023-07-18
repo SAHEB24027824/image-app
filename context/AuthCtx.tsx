437db1ed7f841300e7ddf3917e0a24eaf34fc1aa
Saheb Sutradhar <sahebsutradhar@Sahebs-MacBook-Air.local>
2023-07-18 20:31:13 +0530

    fixed redirect issue

--- context/AuthCtx.tsx
+++ context/AuthCtx.tsx
@@ -2,19 +2,19 @@
 import { MessageAntd } from '@/components/Antd';
 import { Login, Logout } from '@/service/Auth';
 import { MessageService } from '@/util/MessageService';
-import { useRouter,useParams ,usePathname} from 'next/navigation';
+import { useRouter, useParams, usePathname } from 'next/navigation';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-function getCookie(cookieName:any) {
-    return new Promise((resolve,reject)=>{
+function getCookie(cookieName: any) {
+    return new Promise((resolve, reject) => {
         let cookie = {};
         let cookieArray = document.cookie.split(';');
-        cookieArray.forEach(cookie=>{
-            let [key,value] = cookie.split('=');
-            if(key.trim() == cookieName){
+        cookieArray.forEach(cookie => {
+            let [key, value] = cookie.split('=');
+            if (key.trim() == cookieName) {
                 resolve(true)
             }
-            else{
+            else {
                 reject(false)
             }
         })
@@ -24,12 +24,12 @@ function getCookie(cookieName:any) {
 const authContext = createContext<
     {
         isLoggedIn: boolean,
-        LoginFn:(values:any)=>void
-        LogoutFn:()=>void
+        LoginFn: (values: any) => void
+        LogoutFn: () => void
     }>({
         isLoggedIn: false,
-        LoginFn:(values:any) => { },
-        LogoutFn:()=>{}
+        LoginFn: (values: any) => { },
+        LogoutFn: () => { }
     });
 
 export default function AuthContextProvider({ children }: { children: React.ReactNode }) {
@@ -37,12 +37,17 @@ export default function AuthContextProvider({ children }: { children: React.Reac
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
     const path = usePathname()
 
-    const authCheck=async()=>{
+    const authCheck = async () => {
         try {
             const isCookie = await getCookie(process.env.NEXT_PUBLIC_AUTH_COOKIE_NAME)
-            if(isCookie){
+            if (isCookie) {
                 setIsLoggedIn(true)
-                router.push(path)
+                if (path.includes('/login')) {
+                    router.push('/')
+                }
+                else {
+                    router.push(path)
+                }
             }
         } catch (error) {
             setIsLoggedIn(false)
@@ -52,41 +57,41 @@ export default function AuthContextProvider({ children }: { children: React.Reac
 
 
 
-    const LoginFn=async(values:any)=>{
+    const LoginFn = async (values: any) => {
         try {
             const response = await Login(values)
-            if(response){
+            if (response) {
                 MessageAntd.success(MessageService(response));
                 setIsLoggedIn(true)
                 router.push('/')
             }
-            
-        } catch (error:any) {
+
+        } catch (error: any) {
             setIsLoggedIn(false)
             MessageAntd.error(MessageService(error))
         }
     }
 
-    const LogoutFn=async()=>{
+    const LogoutFn = async () => {
         try {
             const response = await Logout()
             MessageAntd.success(MessageService(response));
             setIsLoggedIn(false)
             router.push('/login')
-            
-        } catch (error:any) {
+
+        } catch (error: any) {
             MessageAntd.error(MessageService(error))
         }
     }
 
 
-    useEffect(()=>{
+    useEffect(() => {
         authCheck()
-    },[])
+    }, [])
 
 
     return (
-        <authContext.Provider value={{ isLoggedIn , LoginFn ,LogoutFn}}>
+        <authContext.Provider value={{ isLoggedIn, LoginFn, LogoutFn }}>
             {children}
         </authContext.Provider>
     )
