aa8c4eaa3ada13375c2df675f52b344450bcd4ae
Saheb Sutradhar <sahebsutradhar@Sahebs-MacBook-Air.local>
2023-07-19 17:25:59 +0530

    home page added

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function HomePage() {
+
+export default async function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='flex gap-4 items-center justify-center min-h-screen'>
+
+      <div className='flex gap-4 flex-col '>
+
+        <p className='text-6xl text-sky-600 font-semibold'>Image</p>
+
+        <div className='ml-10 text-8xl text-slate-600 font-bold relative'>
+
+         <span>GALLERY</span>
+
+        <Image src={'/me.png'} alt='me' width={500} height={100}
+         className=' absolute -top-[185px] -right-[205px]'
+        />
+         </div>
+
+
+      </div>
+    </div>
   )
 }
